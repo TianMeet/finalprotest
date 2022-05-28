3e2cb1a285ff63dd4d26ba0621fa23b94e936c65
Tian Meet <1948785447@qq.com>
2022-05-28 21:37:21 +0800

    添加footer部分

--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,4 @@
-$(function(){
-    //向下滑动
+window.addEventListener('load',function(){
     $('.top_nav ul li').hover(function(){
         $(this).children('ul').stop().slideToggle();
     })
@@ -41,8 +40,10 @@ $(function(){
       //tab切换栏制作
       var anibtn = document.querySelector('#animation');
       var carbtn = document.querySelector('#cartoon');
+      var novbtn = document.querySelector('#novel')
       var anibox = document.querySelector('#anima_box');
       var carbox = document.querySelector('#cartn_box');
+      var novbox = document.querySelector('#novel_box')
       anibtn.addEventListener('mouseover',function(){
             this.style.color = 'skyblue';
       })
@@ -52,9 +53,12 @@ $(function(){
       anibtn.addEventListener('click',function(){
             this.classList.add('boder');
             carbtn.classList.remove('boder');
+            novbtn.classList.remove('boder');
             carbtn.style.color = ''
+            novbtn.style.color = ''
             anibox.style.display = 'block';
             carbox.style.display = 'none';
+            novbox.style.display = 'none';
       })
       carbtn.addEventListener('mouseover',function(){
             this.style.color = 'skyblue';
@@ -65,8 +69,28 @@ $(function(){
       carbtn.addEventListener('click',function(){
             this.classList.add('boder');
             anibtn.classList.remove('boder');
-            anibtn.style.color = ''
+            novbtn.classList.remove('boder');
+            anibtn.style.color = '';
+            novbtn.style.color = '';
             anibox.style.display = 'none';
             carbox.style.display = 'block';
+            novbox.style.display = 'none';
       })
+      novbtn.addEventListener('mouseover',function(){
+            this.style.color = 'skyblue';
+      })
+      novbtn.addEventListener('mouseout',function(){
+            this.style.color = '';
+      })
+      novbtn.addEventListener('click',function(){
+        this.classList.add('boder');
+        anibtn.classList.remove('boder');
+        carbtn.classList.remove('boder');
+        anibtn.style.color = ''
+        carbtn.style.color = ''
+        anibox.style.display = 'none';
+        carbox.style.display = 'none';
+        novbox.style.display = 'block';
+      //
+    })
 })
